53cc6fbb0f74794d2d593e622bdaf5ed7c07d2c9
Rangan Roy <ranganroy567@gmail.com>
2021-04-10 23:45:04 +0600

    updated AddPlayer() screen
    Now player can be added to firestore.

--- lib/screens/add_player/add_player_cubit.dart
+++ lib/screens/add_player/add_player_cubit.dart
@@ -1,24 +1,52 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fantasy_cricket/screens/add_player/player_role_cubit.dart';
 
 enum PlayerAddingStatus {adding, added, failed}
 
 class AddPlayerCubit extends Cubit<PlayerAddingStatus> {
   AddPlayerCubit() : super(null);
 
+  // variable for manipulating the from here
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  String playerName, playerRole;
+  
+  // variable for managing state of role field and get player's role
+  final PlayerRoleCubit playerRoleCubit = PlayerRoleCubit();
+  
+  // player's name of the form will be set here
+  String playerName;
 
-  void addPlayerToDb(String playerRole) {
-    print("Player name:"+playerName);
-    print("\n Player role:"+playerRole);
+  Future<void> addPlayerToDb() async {
     if(formKey.currentState.validate()) {
       formKey.currentState.save();
-      Firestore.instance.collection('players').add({
-        'name': playerName,
-        'role': playerRole,
-      }).catchError((error){ print(error); });
+      // after emitting this state a progress animation will be shown on 
+      // AddPlayer() screen
+      emit(PlayerAddingStatus.adding);
+      
+      try {
+        // add player into firestore
+        await Firestore.instance.collection('players').add({
+          'name': playerName,
+          'role': playerRoleCubit.state,
+        });
+
+        // if player is successfully added then null below properties because
+        // after emitting next state the AddPlayer() screen form will be shown   
+        // again where name and role field should be empty
+        playerName = null;
+        playerRoleCubit.emitState(null);
+        
+        // after emitting this state AddPlayer() screen form will be shown again
+        // and a success message will be shown
+        emit(PlayerAddingStatus.added);
+      } catch(error) { 
+        // show error on debug console
+        print(error);
+        // after emitting this state the AddPlayer() screen form will be shown
+        // again with past form values and an error message will be shown
+        emit(PlayerAddingStatus.failed);
+      }
     }
   }
 }
--- lib/screens/add_player/player_role_cubit.dart
+++ lib/screens/add_player/player_role_cubit.dart
@@ -3,12 +3,5 @@ import 'package:bloc/bloc.dart';
 class PlayerRoleCubit extends Cubit<String> {
   PlayerRoleCubit() : super(null);
 
-  final List<String> playerRoles = <String>[
-    'Batsman',
-    'Wicket Keeper',
-    'All Rounder',
-    'Bowler',  
-  ];
-
   void emitState(String state) => emit(state);
 }
