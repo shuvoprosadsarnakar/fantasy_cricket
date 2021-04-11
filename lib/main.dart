fe3489c2077fb4415a4ad5b7e159a1d22fe17618
Shuvo Prosad <shuvoprosad@gmail.com>
2021-04-11 18:53:20 +0600

    Old add player cubit is removed along with the repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:fantasy_cricket/resources/colours/color_pallate.dart';
 import 'package:fantasy_cricket/screens/add_player/add_player.dart';
-import 'package:fantasy_cricket/screens/add_player/cubit/addplayer_cubit.dart';
 import 'package:fantasy_cricket/screens/home/home.dart';
-import 'package:fantasy_cricket/services/database/player_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fantasy Cricket',
-      home: BlocProvider(
-        create: (context) => AddplayersCubit(FsPlayerRepository()),
-        child: AddPlayer(),
-      ),
+      home: AddPlayer(),
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: ColorPallate.mercury,
         accentColor: ColorPallate.pomegranate,
-        canvasColor: ColorPallate.mercury
+        //canvasColor: ColorPallate.mercury,
       ),
       debugShowCheckedModeBanner: false,
     );
--- lib/screens/add_player/add_player.dart
+++ lib/screens/add_player/add_player.dart
@@ -1,195 +1,195 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fantasy_cricket/model/player.dart';
-import 'package:fantasy_cricket/model/player_nr.dart';
 import 'package:fantasy_cricket/resources/colours/color_pallate.dart';
 import 'package:fantasy_cricket/screens/add_player/add_player_cubit.dart';
 import 'package:fantasy_cricket/screens/add_player/player_role_cubit.dart';
-import 'package:fantasy_cricket/services/database/player_repository.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'cubit/addplayer_cubit.dart';
-
 class AddPlayer extends StatelessWidget {
+  // variable for managing state of this screen
   final AddPlayerCubit addPlayerCubit = AddPlayerCubit();
+
+  // All player roles. Every role (String) will used to make dropdown items for
+  // role field of the add player form.
   final List<String> playerRoles = <String>[
     'Batsman',
     'Wicket Keeper',
     'All Rounder',
     'Bowler',
   ];
-
+  
   // dropdown item list for role field
   final List<DropdownMenuItem<String>> playerRoleDropdownList = [];
+
+  // add dropdown items of role field to _playerRoleDropdownList
+  AddPlayer() {
+    playerRoles.forEach((String value) {
+      playerRoleDropdownList.add(DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      ));
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
-    final AddPlayerCubit addPlayerCubit = AddPlayerCubit();
-    final PlayerRoleCubit playerRoleCubit = PlayerRoleCubit();
-    return Scaffold(
-        appBar: AppBar(title: Text('Add Player')),
-        body: BlocBuilder<AddplayersCubit, AddplayerState>(
-          builder: (context, state) {
-            if (state is AddplayerInitial) {
-              return buildForm(addPlayerCubit, playerRoleCubit, context);
-            } else if (state is AddplayerLoading) {
-              print("AddplayerLoading");
-              return buildLoading();
-            } else if (state is AddplayerAdded) {
-              print("AddplayerLoading");
-              return buildLoading();
-            } else {
-              return buildForm(addPlayerCubit, playerRoleCubit, context);
-            }
-          },
-        ));
+    return BlocBuilder<AddPlayerCubit, PlayerAddingStatus>(
+      bloc: addPlayerCubit,
+      builder: (BuildContext context, PlayerAddingStatus playerAddingStatus) {
+        if(addPlayerCubit.state == PlayerAddingStatus.adding) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else return Scaffold(
+          appBar: AppBar(title: Text('Add Player')),
+          body: Form(
+            key: addPlayerCubit.formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 45,
+              ),
+              children: [
+                // name field with field title
+                getNameField(),
+                SizedBox(height: 15),
+                
+                // role field with field title
+                getRoleField(),
+                SizedBox(height: 20),
+
+                // add player button
+                getFormSubmitButton(context),
+              ],
+            ),
+          ),
+        );
+      }
+    );
   }
 
-  Form buildForm(AddPlayerCubit addPlayerCubit, PlayerRoleCubit playerRoleCubit,
-      BuildContext context) {
-    return Form(
-      key: addPlayerCubit.formKey,
-      child: ListView(
-        padding: EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 45,
+  // returns field title
+  Padding getFieldTitle(String title) {
+    return Padding(
+      padding: EdgeInsets.all(4),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: ColorPallate.ebonyClay,
+          fontSize: 20,
         ),
-        children: [
-          // name field
-          Padding(
-            padding: EdgeInsets.all(4),
-            child: Text(
-              "Name",
-              style: TextStyle(
-                color: ColorPallate.ebonyClay,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: ColorPallate.mercury,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 6,
-                horizontal: 8,
-              ),
-              child: TextFormField(
-                initialValue: addPlayerCubit.playerName,
-                cursorColor: Colors.black38,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(fontSize: 16),
-                  hintText: 'Enter name e.g. Sachin Tendulkar',
-                ),
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'Player name is required.';
-                  } else
-                    return null;
-                },
-                onSaved: (String value) {
-                  addPlayerCubit.playerName = value;
-                },
-              ),
-            ),
-          ),
-
-          // space between two fields
-          SizedBox(height: 14),
-
-          // role field
-          Padding(
-            padding: EdgeInsets.all(4),
-            child: Text(
-              "Role",
-              style: TextStyle(
-                color: ColorPallate.ebonyClay,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: ColorPallate.mercury,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 6,
-                  horizontal: 8,
-                ),
-                child: BlocBuilder<PlayerRoleCubit, String>(
-                    bloc: playerRoleCubit,
-                    builder: (BuildContext context, String state) {
-                      return DropdownButtonFormField<String>(
-                        value: playerRoleCubit.state,
-                        isExpanded: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                        hint: Text('Enter role e.g. Batsman'),
-                        items: playerRoles.map((String value) {
-                          return DropdownMenuItem(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (String value) {
-                          playerRoleCubit.emitState(value);
-                        },
-                        validator: (String value) {
-                          if (value == null) {
-                            return 'Select a player role.';
-                          } else
-                            return null;
-                        },
-                        onSaved: (String value) {
-                          //addPlayerCubit.playerRole = value;
-                        },
-                      );
-                    })),
-          ),
-
-          // space between two fields
-          SizedBox(height: 18),
-
-          // add player button
-          TextButton(
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'Add Player',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              primary: Colors.white,
-              backgroundColor: ColorPallate.pomegranate,
-            ),
-            onPressed: () {
-              Playernr player =
-                  Playernr(name: 'Ronit prosad', role: 'Kisu parena');
-              addPlayer(context, player);
-            },
-          )
-        ],
       ),
     );
   }
 
-  void addPlayer(BuildContext context, Playernr player) {
-    final playerCubit = BlocProvider.of<AddplayersCubit>(context);
-    playerCubit.addPlayer(player);
+  // returns name field with field title
+  Column getNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        getFieldTitle('Name'),
+        attachFieldContainer(
+          TextFormField(
+            initialValue: addPlayerCubit.playerName,
+            cursorColor: Colors.black38,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintStyle: TextStyle(fontSize: 16),
+              hintText: 'Enter name',
+            ),
+            validator: (String value) {
+              if(value.isEmpty) {
+                return 'Player name is required.';
+              } else  return null;
+            },
+            onSaved: (String value) {
+              addPlayerCubit.playerName = value;
+            },
+          ),
+        ),
+      ],
+    );
   }
 
-  Widget buildLoading() {
-    return Center(child: CircularProgressIndicator());
+  // returns role field with field title
+  Column getRoleField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        getFieldTitle('Role'),
+        attachFieldContainer(
+          BlocBuilder<PlayerRoleCubit, String>(
+            bloc: addPlayerCubit.playerRoleCubit,
+            builder: (BuildContext context, String state) {
+              return DropdownButtonFormField<String>(
+                value: addPlayerCubit.playerRoleCubit.state,
+                isExpanded: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+                hint: Text('Select a role'),
+                items: playerRoleDropdownList,
+                onChanged: (String value) {
+                  addPlayerCubit.playerRoleCubit.emitState(value);
+                },
+                validator: (String value) {
+                  if(value == null) {
+                    return 'Player role is required.';
+                  } else return null;
+                },
+              );
+            }
+          ),
+        ),
+      ],
+    );
+  }
+
+  // returns add player button
+  TextButton getFormSubmitButton(BuildContext context) {
+    return TextButton(
+      child: Text(
+        'Add Player',
+        style: TextStyle(fontSize: 20),
+      ),
+      style: TextButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.all(15),
+        primary: Colors.white,
+        backgroundColor: ColorPallate.pomegranate,
+      ),
+      onPressed: () async {
+        await addPlayerCubit.addPlayerToDb();
+
+        if(addPlayerCubit.state != null) {
+          String snackBarMsg;
+          
+          if(addPlayerCubit.state == PlayerAddingStatus.added) {
+            snackBarMsg = 'Player added successfully.';
+          } else if(addPlayerCubit.state == PlayerAddingStatus.
+            failed) {
+            snackBarMsg = 'Failed to add player, try again.';
+          }
+
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(snackBarMsg),
+          ));
+        }
+      },
+    );
+  }
+
+  // places a field inside a container and returns the container
+  Container attachFieldContainer(Widget field) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 6,
+        horizontal: 8,
+      ),
+      decoration: BoxDecoration(
+        color: ColorPallate.mercury,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: field,
+    );
   }
 }
