1830f86e42471b6e5daba77a47d0d92348de5d0c
Rangan Roy <ranganroy567@gmail.com>
2021-05-13 12:17:08 +0600

    fixed main.dart file error

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:fantasy_cricket/resources/colours/color_pallate.dart';
 import 'package:fantasy_cricket/routing/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
