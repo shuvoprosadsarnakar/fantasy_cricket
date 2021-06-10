35296fdd696613d92616a1a92bdc540f1ec35475
Rangan Roy <ranganroy567@gmail.com>
2021-06-10 11:55:24 +0600

    updated Exchange model for Steem and Binance payment types

--- lib/models/exchange.dart
+++ lib/models/exchange.dart
@@ -1,8 +1,12 @@
-// keys
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 const String USER_ID_KEY = 'userId';
+const String USERNAME_KEY = 'username';
+const String PAYMENT_USERNAME_KEY = 'paymentUsername';
+const String MEMO_KEY = 'memo';
 const String CHIPS_KEY = 'chips';
+const String TAKA_KEY = 'taka';
+const String STEEM_KEY = 'steem';
 const String PAYMENT_TYPE_KEY = 'paymentType';
 const String MOBILE_KEY = 'mobile';
 const String STATUS_KEY = 'status';
@@ -12,7 +16,12 @@ const String DATE_TIME_KEY = 'dateTime';
 class Exchange {
   String id;
   String userId;
+  String username;
+  String paymentUsername;
+  String memo;
   int chips;
+  double taka;
+  double steem;
   String paymentType;
   String mobile;
   String status;
@@ -22,7 +31,12 @@ class Exchange {
   Exchange({
     this.id,
     this.userId,
+    this.username,
+    this.paymentUsername,
+    this.memo,
     this.chips,
+    this.taka,
+    this.steem,
     this.paymentType,
     this.mobile,
     this.status,
@@ -33,7 +47,12 @@ class Exchange {
   Exchange.fromMap(Map<String, dynamic> doc, String docId) {
     id = docId;
     userId = doc[USER_ID_KEY];
+    username = doc[USERNAME_KEY];
+    paymentUsername = doc[PAYMENT_USERNAME_KEY];
+    memo = doc[MEMO_KEY];
     chips = doc[CHIPS_KEY];
+    taka = doc[TAKA_KEY];
+    steem = doc[STEEM_KEY];
     paymentType = doc[PAYMENT_TYPE_KEY];
     mobile = doc[MOBILE_KEY];
     status = doc[STATUS_KEY];
@@ -42,9 +61,14 @@ class Exchange {
   }
 
   Map<String, dynamic> toMap() {
-    return {
+    return <String, dynamic>{
       USER_ID_KEY: userId,
+      USERNAME_KEY: username,
+      PAYMENT_USERNAME_KEY: paymentUsername,
+      MEMO_KEY: memo,
       CHIPS_KEY: chips,
+      TAKA_KEY: taka,
+      STEEM_KEY: steem,
       PAYMENT_TYPE_KEY: paymentType,
       MOBILE_KEY: mobile,
       STATUS_KEY: status,
