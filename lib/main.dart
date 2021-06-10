52b4214d241bbb9bc2b7a5d7f47b61428030b07c
Rangan Roy <ranganroy567@gmail.com>
2021-06-10 11:46:26 +0600

    set AdminHome page as home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:fantasy_cricket/repositories/auth_repo.dart';
 import 'package:fantasy_cricket/resources/colours/color_pallate.dart';
 import 'package:fantasy_cricket/routing/app_router.dart';
-import 'package:fantasy_cricket/routing/routes.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fantasy_cricket/routing/routes.dart' as routes;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -20,9 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fantasy Cricket',
       onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: AuthRepo.checkUser(),
+      initialRoute: routes.adminHome,
       theme: ThemeData(
-        brightness: Brightness.light,
         primaryColor: ColorPallate.pomegranate,
         accentColor: ColorPallate.pomegranate,
       ),
